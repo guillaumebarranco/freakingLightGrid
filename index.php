f2d8182c2f9ba28c3b1b43b1715a77e4a7d51446
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-02-11 18:03:24 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Grid</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+
+	<body>
+		<h1>My new grid</h1>
+		
+		<div class="grid">
+			<div class="col col-large-6 col-small-12">Hi</div>
+			<div class="col col-large-6 col-small-12">Hi</div>
+			
+			<div class="col col-large-4 col-small-6">Yo</div>
+			<div class="col col-large-4 col-small-6">Yo</div>
+			<div class="col col-large-4 col-small-12">Yo</div>
+			
+			<div class="col col-large-2">Da</div>
+			<div class="col col-large-3">Da</div>
+			<div class="col col-large-5">Da</div>
+			<div class="col col-large-2">Da</div>
+			
+			<div class="col col-large-1 col-small-6">Mj</div>
+			<div class="col col-large-5 col-small-6">Mj</div>
+			<div class="col col-large-5 col-small-12">Mj</div>
+			<div class="col col-large-1 col-small-12">Mj</div>
+		</div>
+
+
+	</body>
+</html>
